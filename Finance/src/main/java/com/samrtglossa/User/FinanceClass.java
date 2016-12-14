cb32b54ca7c559187b04a8dec366a1673f9b8914
venkatachalamd <venkat@smartglossa.com>
2016-12-14 17:17:39 +0530

    adding user servlet in finance

--- Finance/src/main/java/com/samrtglossa/User/FinanceClass.java
+++
@@ -1,5 +0,0 @@
-package com.samrtglossa.User;
-
-public class FinanceClass {
-
-}
--- Finance/src/main/java/com/smartglossa/user/UserServlet.java
+++ Finance/src/main/java/com/smartglossa/user/UserServlet.java
@@ -1,39 +1,69 @@
 package com.smartglossa.user;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UserServlet
- */
+import org.json.JSONObject;
+
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public UserServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String op = request.getParameter("operation");
+		if(op.equals("addUser")){
+			JSONObject obj = new JSONObject();
+			String uname = request.getParameter("uname");
+			String pass = request.getParameter("pass");
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/finance", "root", "root");
+				Statement stmt = conn.createStatement();
+				String query = "insert into user(userName,password) values('"+ uname +"','"+ pass +"')";
+				stmt.execute(query);
+				obj.put("status", "success");
+				
+
+			} catch (Exception e) {
+				obj.put("status", "Failure");
+				e.printStackTrace();
+			}
+			response.getWriter().print(obj);
+	}else if(op.equals("login")){
+		JSONObject obj = new JSONObject();
+		String uname = request.getParameter("user");
+		String pass = request.getParameter("passw");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/finance", "root", "root");
+			Statement stmt = conn.createStatement();
+			String query = "Select *from user where userName='"+ uname +"'AND password="+ pass;
+			ResultSet rs = stmt.executeQuery(query);
+			if(rs.next()){
+				obj.put("user", rs.getString("userName"));
+			}
+		} catch (Exception e) {
+			obj.put("status", "Failure");
+			e.printStackTrace();
+		}
+		response.getWriter().print(obj);
 	}
 
+	}
 }
